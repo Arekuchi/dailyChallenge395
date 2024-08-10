d4411c05b5c766d6cb1bb8e0f372d4143843eab0
Alexis Berthelot <berthelota2@gmail.com>
2024-08-10 14:58:59 +0200

    solution

--- src/main.java
+++ src/main.java
@@ -1,2 +1,26 @@
+
+import java.util.ArrayList;
+
+
+
 public class main {
-}
+    public static void main(String[] args) { int[] nonogramLine = {0,1,1,1,1,1,0,1,1,1,1,0,1,1,1}; consecutiveCounts(nonogramLine); }
+
+    static void consecutiveCounts(int[] line) {
+        ArrayList<Integer> lengthList = new ArrayList<Integer>();
+        int length = 0;
+
+        for(int i = 0; i < line.length; i++) {
+            if(line[i] == 1) {
+                length++;
+            } else {
+                lengthList.add(length);
+                length = 0;
+            }
+        }
+        lengthList.add(length);
+        lengthList.removeIf(n -> (n == 0));
+
+        System.out.println(lengthList);
+    }
+    }
